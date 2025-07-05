d0da9b5b0274fa4efa89a520833981f5cf2d5bdb
tuanvdtd <chuhoi009.dt@gmail.com>
2025-07-05 22:03:51 +0700

    refactor project

--- src/theme.js
+++ src/theme.js
@@ -1,9 +1,27 @@
 import { createTheme } from "@mui/material/styles";
 
 const theme = createTheme({
+  trello: {
+    appBarHeight: "48px",
+    boardBarHeight: "68px",
+  },
   colorSchemes: {
-    light: { palette: { mode: "light" } },
-    dark: { palette: { mode: "dark" } },
+    light: {
+      palette: {
+        mode: "light",
+        primary: {
+          main: "#1976d2", // Example primary color
+        },
+      },
+    },
+    dark: {
+      palette: {
+        mode: "dark",
+        primary: {
+          main: "#bb86fc", // Example primary color
+        },
+      },
+    },
   },
 });
 
