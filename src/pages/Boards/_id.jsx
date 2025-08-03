7dde7290f0df074181766d891c4499671af7c890
tuanvdtd <chuhoi009.dt@gmail.com>
2025-08-03 22:08:35 +0700

    axios: add API call to fetch board details and update Board component

--- src/pages/Boards/_id.jsx
+++ src/pages/Boards/_id.jsx
@@ -1,17 +1,26 @@
 //Board Details Page
+import React, { useState, useEffect } from "react";
 import Container from "@mui/material/Container";
 import AppBar from "~/components/AppBar/AppBar";
 import BoardBar from "~/pages/Boards/BoardBar";
 import BoardContent from "~/pages/Boards/BoardContent/BoardContent.jsx";
-import { mockData } from "~/apis/mockdata";
+// import { mockData } from "~/apis/mockdata";
+import{ fetchBoardDetailsAPI } from "~/apis/index.js";
 
 function Board() {
+  const [board, setBoard] = useState(null);
+  useEffect(() => {
+    const boardId = '688eda486716600a03c62be8';
+    fetchBoardDetailsAPI(boardId).then((board) => {
+      setBoard(board);
+    });
+  }, []);
   return (
     <>
       <Container disableGutters maxWidth={false} sx={{ height: "100vh" }}>
         <AppBar />
-        <BoardBar board={mockData?.board} />
-        <BoardContent board={mockData?.board} />
+        <BoardBar board={board} />
+        <BoardContent board={board} />
       </Container>
     </>
   );
