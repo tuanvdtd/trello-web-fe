00a85cb81695de41b4d895012ef2f6bc931e786a
tuanvdtd <chuhoi009.dt@gmail.com>
2025-09-11 11:26:38 +0700

    deploy fe

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,4 +1,19 @@
-export const API_ROOT = "http://localhost:8017";
+// let apiRoot = ''
+// Lỗi process is not defined vite, config trong vite.config.js
+// if (process.env.BUILD_MODE === 'production') {
+//   apiRoot = 'http://localhost:8017'
+// }
+
+// if (process.env.BUILD_MODE === 'dev') {
+//   apiRoot = 'https://trello-api-express.onrender.com'
+// }
+
+const apiRoot = import.meta.env.DEV 
+  ? 'http://localhost:8017'  // Development
+  : 'https://trello-api-express.onrender.com'  // Production
+
+export const API_ROOT = apiRoot
+
 export const DEFAULT_PAGE = 1;
 export const DEFAULT_ITEMS_PER_PAGE = 12;
 
--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,9 @@ import svgr from "vite-plugin-svgr";
 
 // https://vite.dev/config/
 export default defineConfig({
+  // define: {
+  //    'process.env.BUILD_MODE': JSON.stringify(process.env.BUILD_MODE),
+  // },
   plugins: [react(), svgr()],
   resolve: {
     alias: [{ find: "~", replacement: "/src" }],
